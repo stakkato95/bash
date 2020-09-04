9d659cbf2788e9c805e19fd6e068c08910f0be81
Artsiom Kaliaha <stakkato95@gmail.com>
2020-09-04 15:13:09 +0200

    Update home assignment

--- 5_techno_track/1_user_env/assignment/create-users.bash
+++ 5_techno_track/1_user_env/assignment/create-users.bash
@@ -5,17 +5,21 @@
 # Modified: 04/09/2020
 
 days_since_epoch=$(expr $(date +%s) / 86400)
+max_user_id=$(cat /etc/passwd | awk -F":" '{if($3<65500) print $3}' | sort -n | tail -1)
+
 cat users.csv | tail -n +2 | while read line
 do
+	max_user_id=$(expr $max_user_id + 1)
+
 	# write /etc/passwd
-	passwd_line=$(echo $line | awk -F"," '{printf "%s:x:1001:1000::%s:/bin/bash", $1,$4}')
-	echo $passwd_line >> passwd.txt
+	passwd_line=$(echo $line | awk -F"," -v max_user_id="$max_user_id" '{printf "%s:x:%s:1000::%s:/bin/bash", $1,max_user_id,$4}')
+	echo $passwd_line >> /etc/passwd
 
 	# write /etc/shadow
 	shadow_line=$(echo $line | awk -F"," -v days_since_epoch="$days_since_epoch" '{printf "%s:%s:%s:0:99999:7:::", $1,$3,days_since_epoch}')
-	echo $shadow_line >> shadow.txt
+	echo $shadow_line >> /etc/shadow
 
 	# create home
-	username=$(echo $line | awk -F"," '{print $1}'
+	username=$(echo $line | awk -F"," '{print $1}')
 	mkdir $username
 done
